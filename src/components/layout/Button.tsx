c378cf81c1ac2b9f248d44ab5f9b50a49dd16dce
Julian Müller <3493187+dschu-lab@users.noreply.github.com>
2022-02-12 14:11:38 +0100

    feat: animate button

--- src/components/layout/Button.tsx
+++ src/components/layout/Button.tsx
@@ -1,14 +1,11 @@
-import styled from "styled-components";
+import { animated, useSpring } from "@react-spring/web";
+import styled, { DefaultTheme, useTheme } from "styled-components";
 
 interface StyledButtonProps {
   isActive?: boolean;
   swatch?: SwatchKeys;
 }
-const StyledButton = styled.button<StyledButtonProps>`
-  color: ${({ isActive }) => (isActive ? "white" : "black")};
-  background-color: ${({ theme, swatch = "pink", isActive }) =>
-    isActive ? theme.swatches[swatch].dark : "white"};
-
+const StyledButton = styled(animated.button)<StyledButtonProps>`
   border: 0 none;
   border-radius: 18px;
   padding: 10px;
@@ -23,13 +20,51 @@ const StyledButton = styled.button<StyledButtonProps>`
       cursor: pointer;
     }
   }
-
-  &[disabled] {
-    background: transparent;
-    border-color: gray;
-    border-style: dashed;
-    border-width: 1px;
-  }
 `;
 
-export { StyledButton as Button };
+const springProps = (
+  theme: DefaultTheme,
+  isActive: boolean | undefined,
+  isDisabled: boolean | undefined
+) => ({
+  color: isActive ? theme.inverted : theme.primary,
+  backgroundColor: isDisabled
+    ? "transparent"
+    : isActive
+    ? theme.swatches.pink.dark
+    : theme.inverted,
+  borderColor:
+    isDisabled || !isActive ? theme.inverted : theme.swatches.pink.dark,
+  borderStyle: "dashed",
+  borderWidth: 1,
+});
+
+interface ButtonProps extends StyledButtonProps {
+  children?: React.ReactNode;
+  onClick: () => void;
+  disabled?: boolean;
+  style?: any;
+}
+const Button = ({
+  children,
+  disabled,
+  onClick,
+  isActive,
+  style,
+}: ButtonProps) => {
+  const theme = useTheme();
+
+  const styles = useSpring(springProps(theme, isActive, disabled));
+
+  return (
+    <StyledButton
+      disabled={disabled}
+      onClick={onClick}
+      style={{ ...style, ...styles }}
+    >
+      {children}
+    </StyledButton>
+  );
+};
+
+export { Button };
