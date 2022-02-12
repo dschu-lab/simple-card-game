2d872f12f8611adeeef46eca6ed331d42d66aa49
Julian Müller <3493187+dschu-lab@users.noreply.github.com>
2022-02-12 12:19:40 +0100

    feat: add animated splash screen

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,25 @@
+import { ReactNode, useContext } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { animated, useTransition } from "@react-spring/web";
 import { Controls } from "./Controls";
 import { Details } from "./Details";
 import { Overview } from "./Overview";
-import { AppContextProvider } from "../contexts/AppContext";
+import { AppContext, AppContextProvider } from "../contexts/AppContext";
 import { ErrorMessageDisplay } from "./ErrorMessageDisplay";
 import { theme } from "../style/theme";
-import styled, { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
+import { Heading } from "./layout/Heading";
 
-const AppShell = styled.main`
+const AnimatedLoadingOverlay = styled(animated.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const StyledShell = styled(animated.main)`
   box-sizing: border-box;
   display: grid;
   grid-template-columns: 1fr 1fr;
@@ -16,6 +28,29 @@ const AppShell = styled.main`
   padding: ${({ theme }) => theme.spacing.large};
   height: 100vh;
 `;
+
+const AppShell = ({ children }: { children?: ReactNode }) => {
+  const { cards, requests } = useContext(AppContext);
+
+  const transitions = useTransition(
+    requests.fetching.isActive || cards.length === 0,
+    {
+      from: { opacity: 0 },
+      enter: { opacity: 1 },
+      leave: { opacity: 0 },
+    }
+  );
+
+  return transitions((styles, isFetching) =>
+    isFetching ? (
+      <AnimatedLoadingOverlay style={styles}>
+        <Heading>{"LOADING"}</Heading>
+      </AnimatedLoadingOverlay>
+    ) : (
+      <StyledShell style={styles}>{children}</StyledShell>
+    )
+  );
+};
 
 const App = () => {
   return (
