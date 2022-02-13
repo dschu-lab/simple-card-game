82205ec3d078f8d9c6860b6dbeb2ec53e0b29255
Julian Müller <3493187+dschu-lab@users.noreply.github.com>
2022-02-13 11:13:01 +0100

    test: update tests for app component

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,8 +1,33 @@
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { Card } from "../models/Card";
+import { ApiClient } from "../network/ApiClient";
 import App from "./App";
 
-test("renders without error in loading state", async () => {
-  await render(<App />);
-  const detailsElement = screen.getByText(/Loading/i);
-  expect(detailsElement).toBeInTheDocument();
+jest.mock("../network/ApiClient");
+
+describe("App", () => {
+  beforeEach(() => {
+    (ApiClient as jest.Mock).mockClear();
+
+    (ApiClient.prototype.getCards as jest.Mock).mockReturnValue([
+      new Card("1", "John", "Doe", 1),
+      new Card("2", "Max", "Doe", 2),
+    ]);
+  });
+
+  it("renders without error", async () => {
+    render(<App />);
+
+    await waitFor(async () => {
+      await waitForElementToBeRemoved(() => screen.queryByText("LOADING"));
+    });
+
+    expect(screen.getByText(/John/)).toBeInTheDocument();
+    expect(screen.getByText(/Max/)).toBeInTheDocument();
+  });
 });
